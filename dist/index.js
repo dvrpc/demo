d05fd24a231474804421dc8083b5ddf1d0e5646a
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2017-12-04 17:42:20 -0500

    Fix opn and typos

--- dist/index.js
+++ dist/index.js
@@ -2,18 +2,20 @@
 
 const { exec } = require('child_process')
 const fs = require('fs-extra')
+const opn = require('opn')
+const path = require('path')
 let config = require('./launch.config')
 
 try {
   const userConfig = require(path.join(process.cwd(), 'launch.config'))
   Object.assign(config, userConfig)
-} catch (e) {}
+} catch (e) { console.warn('No user launch.config.js found. Using default config') }
 
 if (!config.projectName.length) {
-  config.projectName = path.dirname(process.cwd()).split(path.sep).pop()
+  config.projectName = path.basename(process.cwd())
 }
 
-const build = () => exec(`npm run build ${config.buildArgs}`, (err, stdout, stderr) => {
+const build = () => exec(`npm run build ${config.buildArgs}`, err => {
   if (err) {
     process.exitCode = 1
     return console.error(err)
@@ -24,8 +26,14 @@ const clean = () => config.clean && fs.emptyDir(`${config.serverPath}${config.pr
 
 const deploy = () => fs.copy(path.join(process.cwd(), config.srcDir), `${config.serverPath}${config.projectName}`)
 
-const launch = () => opn(opts.outHost, {wait: false})
+const launch = () => opn(`${config.serverRootUri}${config.projectName}`, {wait: false})
 
-for (const fn of [build, clean, deploy, launch]) {
-  await fn()
+const init = async () => {
+  console.time('Launch')
+  for (const fn of [build, clean, deploy, launch]) {
+    await fn()
+  }
+  console.timeEnd('Launch')
 }
+
+init()
