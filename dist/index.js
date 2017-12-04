d4e7df53dc7371bc8a9f48337231c97005f9b13d
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2017-12-04 16:10:10 -0500

    Basic implementation

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+const { exec } = require('child_process')
+const fs = require('fs-extra')
+let config = require('./launch.config')
+
+try {
+  const userConfig = require(path.join(process.cwd(), 'launch.config'))
+  Object.assign(config, userConfig)
+} catch (e) {}
+
+if (!config.projectName.length) {
+  config.projectName = path.dirname(process.cwd()).split(path.sep).pop()
+}
+
+const build = () => exec(`npm run build ${config.buildArgs}`, (err, stdout, stderr) => {
+  if (err) {
+    process.exitCode = 1
+    return console.error(err)
+  }
+}).stdout.pipe(process.stdout)
+
+const clean = () => config.clean && fs.emptyDir(`${config.serverPath}${config.projectName}`)
+
+const deploy = () => fs.copy(path.join(process.cwd(), config.srcDir), `${config.serverPath}${config.projectName}`)
+
+const launch = () => opn(opts.outHost, {wait: false})
+
+for (const fn of [build, clean, deploy, launch]) {
+  await fn()
+}
---
+++ dist/launch.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  srcDir: 'dist',
+  serverPath: '\\\\DVRPC-Intranet0\\dev\\'
+  serverRootUri: 'http://intranet.dvrpc.org:8000/',
+  buildArgs: '',
+  projectName: '',
+  clean: true
+}
