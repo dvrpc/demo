f9349d6cbd81c18c24f1a0269744d87becd441a1
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2017-12-14 17:15:39 -0500

    update to v0.5
    * Add yargs (closes #2)
    * Fix exit before promise are fulfilled

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 #! /usr/bin/env node
 
+const argv = require('yargs').command('$0 [projectName]', '', yargs => yargs.options({
+  's': { alias: 'src', nargs: 1 },
+  'd': { alias: 'dest', nargs: 1 },
+  'u': { alias: 'url', nargs: 1 }
+})).argv
 const { execSync } = require('child_process')
 const fs = require('fs-extra')
 const opn = require('opn')
@@ -12,19 +17,31 @@ try {
   Object.assign(config, userConfig)
 } catch (e) { console.warn('No user demo.config.js found. Using default config') }
 
+let args = {
+  srcDir: argv.s || undefined,
+  serverPath: argv.d || undefined,
+  serverRootUri: argv.u || undefined,
+  buildArgs: argv._ || undefined,
+  projectName: argv.projectName || undefined,
+  clean: argv['disable-clean'] ? false : undefined,
+  watch: argv.watch || undefined
+}
+Object.keys(args).forEach(key => (args[key] === undefined) && delete args[key])
+Object.assign(config, args)
+
 if (!config.projectName.length) {
   config.projectName = path.basename(process.cwd())
 }
 
 const build = () => new Promise(resolve => {
   console.log('building')
-  console.log(execSync(`npm run build ${config.buildArgs}`))
+  console.log(execSync(`npm run build ${config.buildArgs}`).toString('utf8'))
   console.log('done building')
   resolve()
 })
 
 const clean = () => new Promise(resolve => {
-  console.log('cleaning')
+  console.log('cleaning ', config.clean)
   config.clean && fs.emptyDirSync(`${config.serverPath}${config.projectName}`)
   console.log('done cleaning')
   resolve()
@@ -39,10 +56,13 @@ const deploy = () => new Promise(resolve => {
 
 const launch = () => new Promise(resolve => {
   console.log('opening')
-  opn(`${config.serverRootUri}${config.projectName}`, {wait: false})
-  console.log('done opening')
-  resolve()
+  opn(`${config.serverRootUri}${config.projectName}`, {wait: false}).then(() => {
+    console.log('done opening')
+    resolve()
+  })
 })
+
+const exit = () => new Promise(resolve => config.watch ? resolve() : process.exit())
 
 function debounce (func, wait) {
   let timeout
@@ -58,7 +78,7 @@ let watchHandler
 
 const init = async () => {
   console.time('Demo')
-  for (const fn of [build, clean, deploy, launch]) {
+  for (const fn of [build, clean, deploy, launch, exit]) {
     await fn()
   }
   console.timeEnd('Demo')
@@ -70,8 +90,6 @@ const init = async () => {
     }), 1000)
     console.log('Now watching...')
   }
-
-  config.watch || process.exit()
 }
 
 init()
